791fdeba7d62753cff1ac9b20eb49da7ef29e2cc
Peter <baaams.park@gmail.com>
2025-01-10 01:44:52 +0900

    20250110

---
+++ src/main/java/peter/park/my_restful_service/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package peter.park.my_restful_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    // GET
+    // URI - /hello-world
+    // @RequestMapping(method=RequestMethod.GET, path="/hello-world")
+
+    @GetMapping(path = "/hello-world")
+    public String helloworld() {
+        return "Hello World";
+    }
+}
