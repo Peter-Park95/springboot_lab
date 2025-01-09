1aacafd94a4e2657fc8e81c24597dfc66879eb20
Peter <baaams.park@gmail.com>
2025-01-10 01:13:51 +0900

    springboot first commit

---
+++ src/main/java/peter/park/my_restful_service/MyRestfulServiceApplication.java
@@ -0,0 +1,20 @@
+package peter.park.my_restful_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class MyRestfulServiceApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ac = SpringApplication.run(MyRestfulServiceApplication.class, args);
+
+		String[] allBeanNames = ac.getBeanDefinitionNames();
+
+		for (String beanName : allBeanNames) {
+			System.out.println(beanName);
+		}
+	}
+
+}
